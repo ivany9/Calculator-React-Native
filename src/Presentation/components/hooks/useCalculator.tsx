9c1dc00f72fd45d7b267e620c529af2e43334d9a
Jorge Ivan Rodriguez <ivany9@gmail.com>
2024-06-22 16:06:16 +1000

    calculater functions

--- src/Presentation/components/hooks/useCalculator.tsx
+++ src/Presentation/components/hooks/useCalculator.tsx
@@ -1,44 +1,166 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+
+
+enum Operator{
+ add,
+ less,
+ multiply,
+ divide,
+
+
+
+}
+
+
+
 
 
 export const useCalculator = () => {
 
-     const [number,setNumber]=useState('0')
+             const [number,setNumber]=useState('0');
+              const[prevnumber,setPrevNumber]=useState('0');
 
-     
+   
+   
+  const lastOperation =useRef<Operator>();
+    
+
+
+   
+
+     const clear =()=>{
+
+       let currentSingn ='';
+       let temporalNumber=number;
+          
+        if(number.includes('-')){
+            
+          console.log('entro al limpiador con un menos');
+           currentSingn='-';
+           temporalNumber=number.substring(1);
+        }
+         if(temporalNumber.length>1){
+          console.log(temporalNumber);
+          return    setNumber(currentSingn+temporalNumber.slice(0,-1))
+            }  
+        setNumber('0')
+          
+     }
+
+     const Borrado=(borrarString:String)=>{
+         
+        if(borrarString==='C')
+          setNumber('0');
+          setPrevNumber('0');
+          console.log('esta borrando');
+        return ;
+
+                                                                  }
+
+     const  toggleSign=()=>{
+      if(number.includes('-')){
+        return setNumber(number.replace('-',''))
+      }
+      setNumber('-'+number);
+                                          }
 
 
      const buildNumber=(numberString:string)=>{
      
-             console.log('number'+number);
-            console.log('numberString'+numberString);
+                                     console.log('number'+number);
+                                      console.log('numberString'+numberString);
 
                                       
-                                      if(numberString===('C'))return setNumber('0') ;
+                  
                                       if(number.includes('.')&&numberString==='.') return;
                                       if(number===('0')&&numberString===('0')) return  console.log('primera condicion');
     
                                      if((number.startsWith('0') || number.startsWith('-0'))&&(numberString==='.')){
                                        console.log('segunda condicion');
                                      return     setNumber(number + numberString);              
-                                     }  
+                                         }  
                                        if(number.startsWith('0')&&!number.includes('.')){ 
                                            console.log('tercera condicion')
                                           return setNumber(numberString)
-                                                 
-                                                                                                              }                                                          
+                                       }      
+                                         if((numberString==='del')&&((number==='0')&&(numberString!='0'))){ 
+                                             console.log('cuarta cindicion')
+                                                return  setNumber('0');          
+                                                                                        }                                                          
                                                               
-                                                                  
-       return setNumber(number+numberString)
+                                                                 
+                                                                          return setNumber(number+numberString);
+    
+                                                                              }
 
-     
-
-
-
-     }
-
-  return {number,
-   buildNumber
-}
-}
+                                                  const setLastNumber=()=>{
+                                                                if(number.endsWith('.')){
+                                                                setPrevNumber(number.slice(0,-1));
+                                                                }else {
+                                                                               setPrevNumber(number);
+                                                                               console.log('entrando a setLAstNumber '+prevnumber);
+                                                                          }
+                                                                            setNumber('0');
   
+                                                                                            }                               
+
+                                                        const divideOperation=()=>{
+                                                                                 setLastNumber();
+                                                                                  lastOperation.current=Operator.divide;
+                                                                                                  }
+
+
+                                                         const addOperation=()=>{
+                                                                                  setLastNumber();
+                                                                                  lastOperation.current=Operator.add;
+                                                                                                 }
+
+                                                         const multiplyOperation=()=>{
+                                                                                   console.log("multiplicacion") ;
+                                                                                   setLastNumber();
+                                                                                  lastOperation.current=Operator.multiply;
+                                                                                                       }
+                                                
+                                                           const lessOperation=()=>{
+                                                                                    setLastNumber();
+                                                                                    lastOperation.current=Operator.less;
+                                                                                                     }
+                               
+                                                               const calculateResult=()=>{
+                                                                  
+                                                                const num1=Number(number);
+                                                                const num2=Number(prevnumber);
+
+                                                                switch(lastOperation.current){
+                                                                          case Operator.add:
+                                                                            setNumber(num1+num2);
+                                                                   
+                                                                   
+                                                                     default:
+                                                                       throw new Error ('undefined Operation');
+
+
+                                                                            
+                                                                        
+
+                                                               }    
+                                                              }                                  
+
+                          
+                        
+
+    return {
+    number,
+    prevnumber,
+   buildNumber,
+   clear,
+   Borrado,
+   toggleSign,
+   divideOperation,
+   addOperation,
+   multiplyOperation,
+   lessOperation,
+   calculateResult
+
+}
+}
--- src/Presentation/components/screens/CalculatorScreen.tsx
+++ src/Presentation/components/screens/CalculatorScreen.tsx
@@ -11,47 +11,65 @@ export const CalculatorScreen = () => {
   
  
   const{ number,
+    prevnumber,
            buildNumber,
+           Borrado,
+           clear,
+           toggleSign,
+           divideOperation,
+          addOperation,
+          multiplyOperation,
+          lessOperation,
+          calculateResult
             }=useCalculator();
+          
+            
 
   return (
     <View style={styles.calculatorContainer}>
 
         <View style={{paddingHorizontal:30,paddingBottom:20}}>
-            <Text  style={styles.mainResult}>{number} </Text> 
-                <Text style={styles.subResult}>15</Text>
+            <Text 
+                   adjustsFontSizeToFit
+                   numberOfLines={1}
+            style={styles.mainResult}>{number} </Text> 
+                <Text 
+                  adjustsFontSizeToFit
+                 numberOfLines={1} 
+                    
+                 style={styles.subResult}>{ (prevnumber==='0')?'':prevnumber}</Text>
         </View>   
                                         
     
        <View style={styles.row}>
            
-       < CalculatorButton onPress={()=>buildNumber("C")}label={"C"}   blackText color={colors.lightGray}   />
-       <CalculatorButton onPress={()=>console.log('+/-')} label={"+/-"} blackText   color={colors.lightGray}     />
-       <CalculatorButton onPress={()=>console.log('del')}label={"del"} blackText    color={colors.lightGray}    />
-       <CalculatorButton onPress={()=>console.log('÷')}label={"÷"}   color={colors.orange}    />
+       < CalculatorButton onPress={()=>Borrado("C")}label={"C"}   blackText color={colors.lightGray}   />
+       <CalculatorButton onPress={ toggleSign} label={"+/-"} blackText   color={colors.lightGray}     />
+       <CalculatorButton onPress={clear}label={"del"} blackText    color={colors.lightGray}    />
+       <CalculatorButton onPress={divideOperation}label={"÷"}   color={colors.orange}    />
         </View>
         <View style={styles.row}>
        <CalculatorButton onPress={()=>buildNumber("7")}label={"7"}     />        
        <CalculatorButton onPress={()=>buildNumber("8")}label={"8"}  />
        <CalculatorButton onPress={()=>buildNumber("9")}label={"9"}      />
-       <CalculatorButton onPress={()=>console.log("x")}label={"x"}   color={colors.orange}    />
+       <CalculatorButton onPress={multiplyOperation}label={"x"}   color={colors.orange}    />
        </View>
        <View style={styles.row}>
        <CalculatorButton onPress={()=>buildNumber("4")}label={"4"}      />        
        <CalculatorButton onPress={()=>buildNumber("5")}label={"5"}    />
        <CalculatorButton onPress={()=>buildNumber("6")}label={"6"}       />
-       <CalculatorButton onPress={()=>console.log("-")}label={"-"}   color={colors.orange}    />
+       <CalculatorButton onPress={lessOperation}label={"-"}   color={colors.orange}    />
        </View>
        <View style={styles.row}>
        <CalculatorButton onPress={()=>buildNumber("1")}label={"1"}      />        
        <CalculatorButton onPress={()=>buildNumber("2")}label={"2"}   />
        <CalculatorButton onPress={()=>buildNumber("3")}label={"3"}     />
-       <CalculatorButton onPress={()=>buildNumber("+")}label={"+"}   color={colors.orange}    />
+       <CalculatorButton onPress={addOperation}label={"+"}   color={colors.orange}    />
        </View>
        <View  style={styles.row}>
     <CalculatorButton onPress={()=>buildNumber("0")}label={"0"}  doublesize />        
        <CalculatorButton onPress={()=>buildNumber(".")}label={"."}     />
-       <CalculatorButton onPress={()=>console.log("=")}label={"="}   color={colors.orange}    />
+       <CalculatorButton onPress={calculateResult}label={"="}   color={colors.orange}    />
         </View>
         </View>
         )}
